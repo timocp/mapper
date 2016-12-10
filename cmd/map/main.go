093ca6fc50a520c9d824f6bb226b634c31acf6f5
Tim Peters <zomg.tim@gmail.com>
2016-12-11 09:58:27 +1030

    Tidy and simplify by composing images for each chunk

--- cmd/map/main.go
+++ cmd/map/main.go
@@ -4,6 +4,7 @@ import "bytes"
 import "fmt"
 import "image"
 import "image/color"
+import "image/draw"
 import "image/png"
 import "log"
 import "os"
@@ -11,15 +12,16 @@ import "os"
 import "github.com/timocp/mapper"
 import "github.com/timocp/mapper/nbt"
 
-type hm struct {
-	x      int
-	z      int
-	values []int32
+// a 16x16 image fragment for a single chunk
+type chunkImage struct {
+	x   int
+	z   int
+	img image.Image
 }
 
 func main() {
 	//fmt.Println("mapper")
-	var heightMaps []hm
+	var images []chunkImage
 	var minx, maxx, minz, maxz int
 	for _, fn := range os.Args[1:] {
 		r := new(mapper.Region)
@@ -27,17 +29,10 @@ func main() {
 		must(r.Open(fn))
 		for x := 0; x < 32; x++ {
 			for z := 0; z < 32; z++ {
-				//offset, length := r.chunk_location(x, z)
-				//fmt.Printf("chunk(%2d, %2d) timestamp=%s offset=%d length=%d\n", x, z, r.chunk_timestamp(x, z), offset, length)
-				//fmt.Printf("Loading chunk %d.%d ... ", x, z)
 				chunk_data, err := r.ChunkData(x, z)
 				must(err)
-				if chunk_data.Len() == 0 {
-					//fmt.Printf("not in file!\n")
-				} else {
-					//fmt.Printf("parsing...\n")
+				if chunk_data.Len() > 0 {
 					chunk := mapper.NewChunk(nbt.Parse(bytes.NewReader(chunk_data.Bytes())), r, x, z)
-					//chunk := mapper.Chunk{nbt.Parse(bytes.NewReader(chunk_data.Bytes())), r.X, r.Z, x, z}
 					if chunk.X() < minx {
 						minx = chunk.X()
 					}
@@ -50,44 +45,38 @@ func main() {
 					if chunk.Z() > maxz {
 						maxz = chunk.Z()
 					}
-					//chunk.Debug()
-					// maybe better than this would be an
-					// array of images, then compose them
-					// at the end
-					heightMaps = append(heightMaps, hm{chunk.X(), chunk.Z(), chunk.HeightMap()})
-					//os.Exit(0)
-					// making an array of all chunks (~76k)
-					// takes too much memory.
+					images = append(images, genHeightImage(chunk))
 				}
 			}
 		}
 	}
-	fmt.Printf("found %d heightMaps...\n", len(heightMaps))
-	fmt.Printf("x: %d..%d\n", minx, maxx)
-	fmt.Printf("z: %d..%d\n", minz, maxz)
-	chunkWidth := maxx - minx + 1
-	chunkHeight := maxz - minz + 1
-	img := image.NewRGBA(image.Rect(0, 0, chunkWidth*16, chunkHeight*16))
-	for _, hm := range heightMaps {
-		// hm.x and hm.z is the xz coord of the chunk, which can be
-		// used to determine the offset into the image to use
-		for i, v := range hm.values {
-			//if i == 10 && hm.x == 10 && hm.z == 10 {
-			//	fmt.Printf("hm.x=%d, hm.z=%d, i=%d, x=%d, z=%d at=(%d,%d) v=%d\n", hm.x, hm.z, i, i%16, i/16, (hm.x-minx)*16+(i%16), (hm.z-minz)*16+(i/16), v)
-			//}
-			img.Set((hm.x-minx)*16+i%16, (hm.z-minz)*16+i/16, color.RGBA{uint8(v), uint8(v), uint8(v), 255})
-		}
+	fmt.Printf("imaged %d chunks (x: %d..%d, z: %d..%d)\n", len(images), minx, maxx, minz, maxz)
+	// create an image large enough to place all the chunks onto
+	img := image.NewRGBA(image.Rect(0, 0, (maxx-minx+1)*16, (maxz-minz+1)*16))
+	// now compose all the mini images into a big one
+	for _, ci := range images {
+		xoffset := (ci.x - minx) * 16
+		zoffset := (ci.z - minz) * 16
+		r := image.Rect(xoffset, zoffset, xoffset+16, zoffset+16)
+		draw.Draw(img, r, ci.img, image.Point{0, 0}, draw.Src)
 	}
 	output, err := os.Create("output.png")
 	defer output.Close()
 	must(err)
 	png.Encode(output, img)
-
-	// 76156 chunks seen in 102 regions
 }
 
 func must(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// return a 16x16 chunkImage where brightness is relative to height
+func genHeightImage(c *mapper.Chunk) chunkImage {
+	img := image.NewRGBA(image.Rect(0, 0, 16, 16))
+	for i, v := range c.HeightMap() {
+		img.Set(i%16, i/16, color.RGBA{uint8(v), uint8(v), uint8(v), 255})
+	}
+	return chunkImage{c.X(), c.Z(), img}
 }
